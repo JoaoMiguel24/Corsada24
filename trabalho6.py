4d770ac11dd5a08ddec2a66ecd6e2acbe909750d
JoaoMiguel24 <161588508+JoaoMiguel24@users.noreply.github.com>
2024-03-20 09:39:59 -0300

    Add files via upload

---
+++ trabalho6.py
@@ -0,0 +1,81 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+4 +4
+8
+
+============ RESTART: C:/Users/202102411742/Desktop/exercicios.py ===========
+João
+
+============ RESTART: C:/Users/202102411742/Desktop/exercicios.py ===========
+João
+nome
+'Joao'
+numero]
+SyntaxError: unmatched ']'
+numero
+7
+numero + 2
+9
+
+a + b
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    a + b
+NameError: name 'a' is not defined
+a + b
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    a + b
+NameError: name 'a' is not defined
+a + b
+Traceback (most recent call last):
+  File "<pyshell#8>", line 1, in <module>
+    a + b
+NameError: name 'a' is not defined
+
+============ RESTART: C:/Users/202102411742/Desktop/exercicios.py ===========
+João
+
+============ RESTART: C:/Users/202102411742/Desktop/exercicios.py ===========
+João
+João
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    João
+NameError: name 'João' is not defined
+numero1
+8
+a media é
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+
+============ RESTART: C:/Users/202102411742/Desktop/exercicios.py ===========
+João
+a media é
+
+============ RESTART: C:/Users/202102411742/Desktop/exercicios.py ===========
+João
+a media é 11.0
+
+============ RESTART: C:/Users/202102411742/Desktop/exercicios.py ===========
+João
+a media é 11.0
+
+============ RESTART: C:/Users/202102411742/Desktop/exercicios.py ===========
+João
+Média aritmética dos números: 11.0
+Quadrado do primeiro número: 64
+Dobro do segundo número: 20
+Quantidade de letras da palavra: 9
+Quantidade de espaços em branco da frase: 5
+O primeiro número é maior que o segundo: False
+O maior número é: 14
+
+============ RESTART: C:/Users/202102411742/Desktop/exercicios.py ===========
+João
+Média aritmética dos números: 11.0
+Quadrado do primeiro número: 64
+Dobro do segundo número: 20
+Quantidade de letras da palavra: 9
+Quantidade de espaços em branco da frase: 5
+O primeiro número é maior que o segundo: False
+O maior número é: 14
