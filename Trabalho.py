602ce4b8c1b6b9dc6f83b55856a2e7068842f917
JoaoMiguel24 <161588508+JoaoMiguel24@users.noreply.github.com>
2024-03-27 10:36:25 -0300

    Trabalho.py

---
+++ Trabalho.py
@@ -0,0 +1,26 @@
+def calcular_soma_media(lista):
+    soma = sum(lista)
+    media = soma / len(lista)
+    return soma, media
+
+numeros = [1, 2, 3, 4]
+soma, media = calcular_soma_media(numeros)
+print("Soma:", soma)
+print("Média:", media)
+
+def substituir_palavra(lista, palavra_antiga, palavra_nova):
+    return [palavra_nova if palavra == palavra_antiga else palavra for palavra in lista]
+
+
+lista_palavras = ["Botafogo", "Fogo", "Tiquinho", "Incendeia"]
+palavra_antiga = "Fogo"
+palavra_nova = "Lobo"
+lista_alterada = substituir_palavra(lista_palavras, palavra_antiga, palavra_nova)
+print(lista_alterada)
+
+def imprimir_triangulo(num_linhas):
+    for i in range(1, num_linhas + 1):
+        print(" " * (num_linhas - i) + "*" * (2*i - 1))
+
+num_linhas = 5
+imprimir_triangulo(num_linhas)
